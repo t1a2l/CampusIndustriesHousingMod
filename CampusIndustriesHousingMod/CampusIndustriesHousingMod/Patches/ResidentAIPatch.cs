57e000105b0a71074cfb78eaa2f1f650b4268716
Tal <talrofeh@gmail.com>
2022-08-14 03:43:38 +0300

    no moving or making babies while in the dorms

---
+++ CampusIndustriesHousingMod/CampusIndustriesHousingMod/Patches/ResidentAIPatch.cs
@@ -0,0 +1,53 @@
+﻿using ColossalFramework;
+using HarmonyLib;
+
+namespace CampusIndustriesHousingMod
+{
+    [HarmonyPatch(typeof(ResidentAI))]
+    class ResidentAIPatch
+    {
+
+        [HarmonyPatch(typeof(ResidentAI), "CanMakeBabies")]
+        [HarmonyPrefix]
+        public static bool CanMakeBabies(uint citizenID, ref Citizen data, ref bool __result)
+        {
+            BuildingManager buildingManager = Singleton<BuildingManager>.instance;
+            Building homeBuilding = buildingManager.m_buildings.m_buffer[data.m_homeBuilding];
+            if(homeBuilding.Info.m_buildingAI is DormsAI)
+            {
+                __result = false;
+                return false;
+            } 
+
+            return true;
+        }
+
+        [HarmonyPatch(typeof(ResidentAI), "TryMoveAwayFromHome")]
+        [HarmonyPrefix]
+        public static bool TryMoveAwayFromHome(uint citizenID, ref Citizen data)
+        {
+            BuildingManager buildingManager = Singleton<BuildingManager>.instance;
+            Building homeBuilding = buildingManager.m_buildings.m_buffer[data.m_homeBuilding];
+            if(homeBuilding.Info.m_buildingAI is DormsAI)
+            {
+                return false;
+            } 
+
+            return true;
+        }
+
+        [HarmonyPatch(typeof(ResidentAI), "TryFindPartner")]
+        [HarmonyPrefix]
+        public static bool TryFindPartner(uint citizenID, ref Citizen data)
+        {
+            BuildingManager buildingManager = Singleton<BuildingManager>.instance;
+            Building homeBuilding = buildingManager.m_buildings.m_buffer[data.m_homeBuilding];
+            if(homeBuilding.Info.m_buildingAI is DormsAI)
+            {
+                return false;
+            } 
+
+            return true;
+        }
+    }
+}
