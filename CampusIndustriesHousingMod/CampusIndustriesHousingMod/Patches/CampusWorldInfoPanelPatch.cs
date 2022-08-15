1205c416a77c6761a8e847ea7a3ddf7faebb930e
Tal <talrofeh@gmail.com>
2022-08-16 02:24:19 +0300

    error fixes

--- CampusIndustriesHousingMod/CampusIndustriesHousingMod/Patches/CampusWorldInfoPanelPatch.cs
+++ CampusIndustriesHousingMod/CampusIndustriesHousingMod/Patches/CampusWorldInfoPanelPatch.cs
@@ -9,33 +9,65 @@ namespace CampusIndustriesHousingMod
     [HarmonyPatch(typeof(CampusWorldInfoPanel))]
     class CampusWorldInfoPanelPatch
     {
+        private static readonly bool LOG_CAMPUS = true;
+
         [HarmonyPatch(typeof(CampusWorldInfoPanel), "UpdateBindings")]
         [HarmonyPostfix]
         public static void UpdateBindings(CampusWorldInfoPanel __instance)
         {
             var m_InstanceID = (InstanceID)typeof(WorldInfoPanel).GetField("m_InstanceID", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance);
 
+            if(m_InstanceID.Park == 0)
+            {
+                Logger.logInfo(LOG_CAMPUS, "Campus m_InstanceID Park is 0");
+                return;
+            }
+
             DistrictManager instance = Singleton<DistrictManager>.instance;
             BuildingManager buildingManager = Singleton<BuildingManager>.instance;
+
+            var campus_buildings = instance.m_parks.m_buffer[m_InstanceID.Park].GetCampusBuildings(m_InstanceID.Park);
+
             var total_ocuppied_apartment_num = 0;
             var total_apartment_num = 0;
 
-            for (uint i=0; instance.m_parks.m_buffer[m_InstanceID.Park].m_buildings.m_buffer.Length > i; ++i) {
-                var buildingId = instance.m_parks.m_buffer[m_InstanceID.Park].m_buildings.m_buffer[i];
+            Logger.logInfo(LOG_CAMPUS, "Campus Buildings number: {0}", campus_buildings.m_size);
+
+            for (ushort i = 0; i < campus_buildings.m_size; i++) 
+            {
+                var buildingId = campus_buildings[i];
+                Logger.logInfo(LOG_CAMPUS, "Campus Building id: {0}", buildingId);
+                if(buildingId == 0)
+                {
+                    continue;
+                }
                 Building campusBuilding = buildingManager.m_buildings.m_buffer[buildingId];
                 if(campusBuilding.Info.GetAI() is DormsAI dormsAI)
                 {
                     dormsAI.getOccupancyDetails(ref campusBuilding, out int numResidents, out int numApartmentsOccupied);
                     total_ocuppied_apartment_num += numApartmentsOccupied;
                     total_apartment_num += dormsAI.getModifiedCapacity();
+                    Logger.logInfo(LOG_CAMPUS, "Campus Building occupied apartments: {0}", numApartmentsOccupied);
                 }
             }
 
             var dorms_capacity = StringUtils.SafeFormat("Dorms Apartment Capacity: {0} / {1}", total_ocuppied_apartment_num, total_apartment_num);
 
+            Logger.logInfo(LOG_CAMPUS, "Dorms Apartment Capacity: {0} / {1}", total_ocuppied_apartment_num, total_apartment_num);
+
             var m_studentCapacityLabel = (UILabel)typeof(CampusWorldInfoPanel).GetField("m_studentCapacityLabel", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance);
 
-            m_studentCapacityLabel.text = m_studentCapacityLabel.text + Environment.NewLine + dorms_capacity;
+            if(m_studentCapacityLabel == null)
+            {
+                Logger.logInfo(LOG_CAMPUS, "m_studentCapacityLabel is null");
+                return;
+            }
+
+            Logger.logInfo(LOG_CAMPUS, "old campus worker label: {0}", m_studentCapacityLabel);
+
+            m_studentCapacityLabel.text = m_studentCapacityLabel.text + Environment.NewLine + dorms_capacity + Environment.NewLine;
+
+            Logger.logInfo(LOG_CAMPUS, "new campus worker label: {0}", m_studentCapacityLabel);
 
         }
 
--- CampusIndustriesHousingMod/CampusIndustriesHousingMod/Patches/IndustryWorldInfoPanelPatch.cs
+++ CampusIndustriesHousingMod/CampusIndustriesHousingMod/Patches/IndustryWorldInfoPanelPatch.cs
@@ -9,35 +9,94 @@ namespace CampusIndustriesHousingMod
     [HarmonyPatch(typeof(IndustryWorldInfoPanel))]
     class IndustryWorldInfoPanelPatch
     {
+        private static readonly bool LOG_INDUSTRY = true;
+
         [HarmonyPatch(typeof(IndustryWorldInfoPanel), "UpdateWorkersAndTotalUpkeep")]
         [HarmonyPostfix]
         public static void UpdateWorkersAndTotalUpkeep(IndustryWorldInfoPanel __instance)
         {
             var m_InstanceID = (InstanceID)typeof(WorldInfoPanel).GetField("m_InstanceID", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance);
 
-            DistrictManager instance = Singleton<DistrictManager>.instance;
+            if(m_InstanceID.Park == 0)
+            {
+                Logger.logInfo(LOG_INDUSTRY, "Industry m_InstanceID Park is 0");
+                return;
+            }
+
             BuildingManager buildingManager = Singleton<BuildingManager>.instance;
+
+            var industry_buildings = GetIndustryBuildings(m_InstanceID.Park);
+
             var total_ocuppied_apartment_num = 0;
             var total_apartment_num = 0;
 
-            for (uint i=0; instance.m_parks.m_buffer[m_InstanceID.Park].m_buildings.m_buffer.Length > i; ++i) {
-                var buildingId = instance.m_parks.m_buffer[m_InstanceID.Park].m_buildings.m_buffer[i];
+            Logger.logInfo(LOG_INDUSTRY, "Industry Buildings number: {0}", industry_buildings.m_size);
+
+            for (ushort i = 0; i < industry_buildings.m_size; i++) 
+            {
+                var buildingId = industry_buildings[i];
+                Logger.logInfo(LOG_INDUSTRY, "Industry Building id: {0}", buildingId);
+                if(buildingId == 0)
+                {
+                    continue;
+                }
                 Building industryBuilding = buildingManager.m_buildings.m_buffer[buildingId];
                 if(industryBuilding.Info.GetAI() is BarracksAI barracksAI)
                 {
                     barracksAI.getOccupancyDetails(ref industryBuilding, out int numResidents, out int numApartmentsOccupied);
                     total_ocuppied_apartment_num += numApartmentsOccupied;
                     total_apartment_num += barracksAI.getModifiedCapacity();
+                    Logger.logInfo(LOG_INDUSTRY, "Industry Building occupied apartments: {0}", numApartmentsOccupied);
                 }
             }
 
             var barracks_capacity = StringUtils.SafeFormat("Barracks Apartment Capacity: {0} / {1}", total_ocuppied_apartment_num, total_apartment_num);
 
+            Logger.logInfo(LOG_INDUSTRY, "Barracks Apartment Capacity: {0} / {1}", total_ocuppied_apartment_num, total_apartment_num);
+
             var m_workersInfoLabel = (UILabel)typeof(IndustryWorldInfoPanel).GetField("m_workersInfoLabel", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance);
 
-            m_workersInfoLabel.text = m_workersInfoLabel.text + Environment.NewLine + barracks_capacity;
+            Logger.logInfo(LOG_INDUSTRY, "old Industry worker label: {0}", m_workersInfoLabel);
+
+            m_workersInfoLabel.text = m_workersInfoLabel.text + Environment.NewLine + barracks_capacity + Environment.NewLine;
+
+            Logger.logInfo(LOG_INDUSTRY, "new Industry worker label: {0}", m_workersInfoLabel);
 
         }
+
+        private static FastList<ushort> GetIndustryBuildings(byte industry)
+        {
+            var buildings = new FastList<ushort>();
+            FastList<ushort> serviceBuildings = Singleton<BuildingManager>.instance.GetServiceBuildings(ItemClass.Service.PlayerIndustry);
+		    for (int i = 0; i < serviceBuildings.m_size; i++)
+		    {
+			    IndustryBuildingAI industryBuildingAI = Singleton<BuildingManager>.instance.m_buildings.m_buffer[serviceBuildings[i]].Info.m_buildingAI as IndustryBuildingAI;
+			    if (industryBuildingAI != null && GetArea(ref Singleton<BuildingManager>.instance.m_buildings.m_buffer[serviceBuildings[i]]) == industry)
+			    {
+				    buildings.Add(serviceBuildings[i]);
+			    }
+		    }
+            return buildings;
+        }
+
+        private static byte GetArea(ref Building data)
+		{
+			DistrictManager instance = Singleton<DistrictManager>.instance;
+			byte b = instance.GetPark(data.m_position);
+			IndustryBuildingAI industryBuildingAI = data.Info.m_buildingAI as IndustryBuildingAI;
+			if (b != 0)
+			{
+				if (!instance.m_parks.m_buffer[b].IsIndustry)
+				{
+					b = 0;
+				}
+				else if (industryBuildingAI != null && (industryBuildingAI.m_industryType == DistrictPark.ParkType.Industry || industryBuildingAI.m_industryType != instance.m_parks.m_buffer[b].m_parkType))
+				{
+					b = 0;
+				}
+			}
+			return b;
+		}
 
     }
 }
