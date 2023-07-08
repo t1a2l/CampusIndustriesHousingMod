7b1980fe189915b4eef5db5f57f1af54b65c0e45
Tal <talrofeh@gmail.com>
2023-07-08 07:55:36 +0300

    add patch to show checkbox

---
+++ CampusIndustriesHousingMod/CampusIndustriesHousingMod/Patches/CityServiceWorldInfoPanelPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace CampusIndustriesHousingMod.Patches
+{
+    [HarmonyPatch(typeof(CityServiceWorldInfoPanel))]
+	public static class CityServiceWorldInfoPanelPatch
+	{
+		[HarmonyPatch(typeof(CampusWorldInfoPanel), "OnSetTarget")]
+        [HarmonyPostfix]
+        public static void OnSetTarget_Postfix(CityServiceWorldInfoPanel __instance)
+        {
+            CampusIndustriesHousingMod.Panel?.RefreshData();
+        }
+	}
+}
