e8dc5ef4083f081a98373fe919dfce1e05c6ee3d
Tal <talrofeh@gmail.com>
2023-07-17 18:15:56 +0300

    patches to fix not counting students/workers

--- CampusIndustriesHousingMod/CampusIndustriesHousingMod/Patches/SchoolAIPatch.cs
+++ CampusIndustriesHousingMod/CampusIndustriesHousingMod/Patches/SchoolAIPatch.cs
@@ -7,20 +7,20 @@ namespace CampusIndustriesHousingMod.Patches
 	[HarmonyPatch(typeof(SchoolAI))]
 	public static class SchoolAIPatch
 	{
-		private delegate void PlayerBuildingAICreateBuildingDelegate(SchoolAI __instance, ushort buildingID, ref Building data);
+		private delegate void PlayerBuildingAICreateBuildingDelegate(PlayerBuildingAI __instance, ushort buildingID, ref Building data);
         private static PlayerBuildingAICreateBuildingDelegate BaseCreateBuilding = AccessTools.MethodDelegate<PlayerBuildingAICreateBuildingDelegate>(typeof(PlayerBuildingAI).GetMethod("CreateBuilding", BindingFlags.Instance | BindingFlags.Public), null, false);
 
-		private delegate void PlayerBuildingAIBuildingLoadedDelegate(SchoolAI __instance, ushort buildingID, ref Building data, uint version);
+		private delegate void PlayerBuildingAIBuildingLoadedDelegate(PlayerBuildingAI __instance, ushort buildingID, ref Building data, uint version);
         private static PlayerBuildingAIBuildingLoadedDelegate BaseBuildingLoaded = AccessTools.MethodDelegate<PlayerBuildingAIBuildingLoadedDelegate>(typeof(PlayerBuildingAI).GetMethod("BuildingLoaded", BindingFlags.Instance | BindingFlags.Public), null, false);
 
-		private delegate void PlayerBuildingAIEndRelocatingDelegate(SchoolAI __instance, ushort buildingID, ref Building data);
+		private delegate void PlayerBuildingAIEndRelocatingDelegate(PlayerBuildingAI __instance, ushort buildingID, ref Building data);
         private static PlayerBuildingAIEndRelocatingDelegate BaseEndRelocating = AccessTools.MethodDelegate<PlayerBuildingAIEndRelocatingDelegate>(typeof(PlayerBuildingAI).GetMethod("EndRelocating", BindingFlags.Instance | BindingFlags.Public), null, false);
 
 		[HarmonyPatch(typeof(SchoolAI), "CreateBuilding")]
         [HarmonyPrefix]
         public static bool CreateBuilding(SchoolAI __instance, ushort buildingID, ref Building data)
         {
-			if(data.Info.GetAI() is DormsAI || data.Info.GetAI() is BarracksAI)
+			if(data.Info.GetAI() is DormsAI)
 			{
 				BaseCreateBuilding(__instance, buildingID, ref data);
 				return false;
@@ -32,7 +32,7 @@ namespace CampusIndustriesHousingMod.Patches
         [HarmonyPrefix]
 		public static bool BuildingLoaded(SchoolAI __instance, ushort buildingID, ref Building data, uint version)
 		{
-			if(data.Info.GetAI() is DormsAI || data.Info.GetAI() is BarracksAI)
+			if(data.Info.GetAI() is DormsAI)
 			{
 				BaseBuildingLoaded(__instance, buildingID, ref data, version);
 				return false;
@@ -44,7 +44,7 @@ namespace CampusIndustriesHousingMod.Patches
         [HarmonyPrefix]
 		public static bool EndRelocating(SchoolAI __instance, ushort buildingID, ref Building data)
 		{
-			if(data.Info.GetAI() is DormsAI || data.Info.GetAI() is BarracksAI)
+			if(data.Info.GetAI() is DormsAI)
 			{
 				BaseEndRelocating(__instance, buildingID, ref data);
 				return false;
