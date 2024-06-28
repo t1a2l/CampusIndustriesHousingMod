d56501c79a495f0452d3f1a2d5c335b2ab6f8a3f
Tal <talrofeh@gmail.com>
2024-06-29 02:18:38 +0300

    fix colors not showing for health and population

---
+++ CampusIndustriesHousingMod/CampusIndustriesHousingMod/Patches/PlayerBuildingAIPatch.cs
@@ -0,0 +1,29 @@
+﻿using CampusIndustriesHousingMod.AI;
+using HarmonyLib;
+
+namespace CampusIndustriesHousingMod.Patches
+{
+    [HarmonyPatch(typeof(PlayerBuildingAI))]
+	public static class PlayerBuildingAIPatch
+    {
+        [HarmonyPatch(typeof(PlayerBuildingAI), "SimulationStepActive")]
+        [HarmonyPrefix]
+        public static void Prefix(PlayerBuildingAI __instance, ushort buildingID, ref Building buildingData, ref Building.Frame frameData, ref byte __state)
+        {
+            if (buildingData.Info.GetAI() is DormsAI || buildingData.Info.GetAI() is BarracksAI)
+            {
+                __state = buildingData.m_citizenCount;
+            } 
+        }
+
+        [HarmonyPatch(typeof(PlayerBuildingAI), "SimulationStepActive")]
+        [HarmonyPostfix]
+        public static void Postfix(PlayerBuildingAI __instance, ushort buildingID, ref Building buildingData, ref Building.Frame frameData, ref byte __state)
+        {
+            if (buildingData.Info.GetAI() is DormsAI || buildingData.Info.GetAI() is BarracksAI)
+            {
+                buildingData.m_citizenCount = __state;
+            }
+        }
+    }
+}
