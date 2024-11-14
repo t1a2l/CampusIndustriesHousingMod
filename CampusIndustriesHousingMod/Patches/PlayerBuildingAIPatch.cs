5d57cffd5dd5887a92f4fe520e39266275c89cde
Tal <talrofeh@gmail.com>
2024-11-15 00:59:08 +0200

    use the AI to create and load buildings instead of the patch

--- CampusIndustriesHousingMod/Patches/PlayerBuildingAIPatch.cs
+++ CampusIndustriesHousingMod/Patches/PlayerBuildingAIPatch.cs
@@ -7,50 +7,6 @@ namespace CampusIndustriesHousingMod.Patches
     [HarmonyPatch(typeof(PlayerBuildingAI))]
 	public static class PlayerBuildingAIPatch
     {
-        [HarmonyPatch(typeof(PlayerBuildingAI), "CreateBuilding")]
-        [HarmonyPrefix]
-        public static void CreateBuildingPrefix(PlayerBuildingAI __instance, ushort buildingID, ref Building data)
-        {
-            if (data.Info.GetAI() is BarracksAI barracksAI)
-            {
-                var buildingRecord = HousingManager.CreateBuildingRecord(buildingID);
-                barracksAI.numApartments = buildingRecord.NumOfApartments;
-            }
-            else if (data.Info.GetAI() is DormsAI dormsAI)
-            {
-                var buildingRecord = HousingManager.CreateBuildingRecord(buildingID);
-                dormsAI.numApartments = buildingRecord.NumOfApartments;
-            }
-        }
-
-        [HarmonyPatch(typeof(PlayerBuildingAI), "BuildingLoaded")]
-        [HarmonyPrefix]
-        public static void BuildingLoadedPrefix(PlayerBuildingAI __instance, ushort buildingID, ref Building data)
-        {
-            if(data.Info.GetAI() is BarracksAI || data.Info.GetAI() is DormsAI)
-            {
-                HousingManager.BuildingRecord buildingRecord;
-
-                if (!HousingManager.BuildingRecordExist(buildingID))
-                {
-                    buildingRecord = HousingManager.CreateBuildingRecord(buildingID);
-                }
-                else
-                {
-                    buildingRecord = HousingManager.GetBuildingRecord(buildingID);
-                }
-
-                if (data.Info.GetAI() is BarracksAI barracksAI)
-                {
-                    barracksAI.numApartments = buildingRecord.NumOfApartments;
-                }
-                else if (data.Info.GetAI() is DormsAI dormsAI)
-                {
-                    dormsAI.numApartments = buildingRecord.NumOfApartments;
-                }
-            }
-        }
-
         [HarmonyPatch(typeof(PlayerBuildingAI), "SimulationStepActive")]
         [HarmonyPrefix]
         public static void SimulationStepActivePrefix(PlayerBuildingAI __instance, ushort buildingID, ref Building buildingData, ref Building.Frame frameData, ref byte __state)
