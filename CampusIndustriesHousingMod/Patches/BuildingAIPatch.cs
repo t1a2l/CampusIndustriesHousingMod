e7050b2460f68952f35c7038c0990c2a649f3005
Tal <talrofeh@gmail.com>
2025-12-23 03:06:15 +0200

    remove industry workers from barracks

---
+++ CampusIndustriesHousingMod/Patches/BuildingAIPatch.cs
@@ -0,0 +1,20 @@
+﻿using CampusIndustriesHousingMod.AI;
+using HarmonyLib;
+
+namespace CampusIndustriesHousingMod.Patches
+{
+    [HarmonyPatch(typeof(BuildingAI))]
+    public static class BuildingAIPatch
+    {
+        [HarmonyPatch(typeof(BuildingAI), "SpawnAnimals")]
+        [HarmonyPrefix]
+        public static bool SpawnAnimals(ushort buildingID, ref Building data)
+        {
+            if (data.Info.GetAI() is BarracksAI)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ CampusIndustriesHousingMod/Patches/LivestockAIPatch.cs
@@ -0,0 +1,27 @@
+﻿using CampusIndustriesHousingMod.AI;
+using ColossalFramework;
+using HarmonyLib;
+
+namespace CampusIndustriesHousingMod.Patches
+{
+    [HarmonyPatch(typeof(LivestockAI))]
+    public static class LivestockAIPatch
+    {
+        [HarmonyPatch(typeof(LivestockAI), "LoadInstance")]
+        [HarmonyPrefix]
+        public static bool LoadInstance(ushort instanceID, ref CitizenInstance data)
+        {
+            if(data.m_targetBuilding != 0)
+            {
+                var buildingAI = Singleton<BuildingManager>.instance.m_buildings.m_buffer[data.m_targetBuilding].Info.m_buildingAI;
+                if (buildingAI is BarracksAI)
+                {
+                    Singleton<CitizenManager>.instance.ReleaseCitizenInstance(instanceID);
+                    Singleton<CitizenManager>.instance.ReleaseCitizen(data.m_citizen);
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
